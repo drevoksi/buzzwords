33063e34cb16b1c34fca57a3ec3342b207e23711
drevoksi <dawwwst@gmail.com>
2025-03-15 13:15:42 +0000

    feat: implement Action.py for website call

---
+++ Action.py
@@ -0,0 +1,7 @@
+from BuzzStatement import BuzzStatement
+
+buzz = BuzzStatement()
+
+@app.route('/get-buzz-statement',methods=['POST'])
+def get_buzz_statement(file_path):
+    buzz.get_buzz_statement(file_path)
